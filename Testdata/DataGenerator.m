db3f5ed058ab107595a10f7adc3f84f667ae0561
Christoph Bauinger <c.bauinger@gmx.at>
2019-05-30 15:09:11 -0700

    Added matlab files

---
+++ Testdata/DataGenerator.m
@@ -0,0 +1,18 @@
+clear
+clc
+close all
+
+N = 100;
+A = zeros(N, N);
+
+for i = 1:N
+    for j = 1:N
+        if (sqrt( (N/2 - i)^2 + (N/2 - j)^2) < 3*N/8)
+            A(i, j) = 1;            
+        end
+    end
+end
+
+dlmwrite('test.txt',A,'delimiter',' ','precision',1)
+
+    
